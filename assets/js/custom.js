eb59b1f6cf1e8122355dd5ccb67a2f6939dd8337
Sallah Dari <sallah.dari@hva.nl>
2024-04-17 20:04:08 +0200

    contact page

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -15,3 +15,23 @@ function showSlides(n) {
   }
   slides[slideIndex].style.display = "block";
 }
+
+(function() {
+  // https://dashboard.emailjs.com/admin/account
+  emailjs.init({
+    publicKey: "9zup4SY1N6gwRIKbq",
+  });
+})();
+
+window.onload = function() {
+  document.getElementById('contact-form').addEventListener('submit', function(event) {
+      event.preventDefault();
+      // these IDs from the previous steps
+      emailjs.sendForm('contact_service', 'contact_form', this)
+          .then(() => {
+              console.log('SUCCESS!');
+          }, (error) => {
+              console.log('FAILED...', error);
+          });
+  });
+}
